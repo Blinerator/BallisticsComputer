52c907e80eeb3d7e25e4f28f6b190b7d8c1dd3de
Ilya <ilya.cable1@gmail.com>
2023-03-02 12:22:52 -0800

    Re-organised and cleaned up, added new maths func.
    Re-wrote the main trajectory calculator to a series of functions accessible from main.py; added functionality for negative pitch trajectory calculations (e.g., the target is below the cannon); added other math functions for calculating various angles etc... necessary for the system.

--- Python Code/TrajectoryCalculator.py
+++ Python Code/TrajectoryCalculator.py
@@ -1,20 +1,7 @@
+from logging import exception
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-from serial import Serial
-
-ser=Serial(port='COM1', baudrate=9600)
-
-
-os.system('CLS')
-
-initial_velocity=55
-rocket_radius=21.6/1000 #rocket radius in meters
-rocket_mass=.133 #sabot mass in kg
-
-sample_resolution=0.01
-drag_coeff=0.15 #taken from "Golf Ball Flight Dynamics" by Brett Burglund and Ryan Street
 
 def eulers_method_x(p,v_x,D,r,m,time_interval,displacement,time_till_touchdown):
     #print("Initializing Euler's Method for 'x' displacement...")
@@ -32,7 +19,7 @@ def eulers_method_x(p,v_x,D,r,m,time_interval,displacement,time_till_touchdown):
     #print("Calculations completed successfully.  Please refer to text document for values.")
     return array_x, v_x
 
-def eulers_method(p,v,D,r,m,time_interval,displacement,elevation):
+def eulers_method(p,v,D,r,m,time_interval,displacement,elevation,end_elev):
     #print("Initializing Euler's Method for 'y' displacement...")
     start_displacement=displacement
     y_array=[]
@@ -42,7 +29,7 @@ def eulers_method(p,v,D,r,m,time_interval,displacement,elevation):
     v_x=v*math.cos(math.radians(elevation))
     v_x_forprint=v_x
     max=0
-    while(displacement>=start_displacement):
+    while(displacement>=end_elev):
         a=(-0.5*(p*v_y*v_y*D*3.1415*r*r)-(m*9.81))/m  #acceleration equation, this time with gravity
          
         displacement=displacement+(v_y*time_interval+0.5*a*time_interval*time_interval) #this adds the displacement from each time interval to the last one
@@ -61,60 +48,68 @@ def eulers_method(p,v,D,r,m,time_interval,displacement,elevation):
     #print("Calculations completed successfully.  Please refer to text document for values.")
     return time_till_touchdown*time_interval, x_array[time_till_touchdown-1], x_array, y_array, v_x_forprint, v_y_forprint, max, v_y, v_x
 
-ideal_x=int(input("Enter desired range"))
-ideal_y=int(input("Enter desired height"))
-dir_ind=str(input("Select direct fire (d) or indirect fire (i)"))
+def return_angle(ideal_x,ideal_y,dir_ind,initial_velocity,drag_coeff,rocket_radius,rocket_mass,sample_resolution,y_displacement,p):
+    if(ideal_y<0):
+        end_elev=ideal_y-1
+    else: end_elev=0
 
-if dir_ind == 'd': #max range is achieved at around 40 degrees, therefore anything greater will be indirect fire
-    start_angle=0
-    end_angle=37.8
-else:
-    start_angle=37.8
-    end_angle=90
+    prev_margin=np.inf
+    if dir_ind == 'd': #max range is achieved at around 40 degrees, therefore anything greater will be indirect fire
+        start_angle=0
+        end_angle=37.5
+    elif dir_ind == 'i':
+        start_angle=37
+        end_angle=90
+    else:
+        raise Exception(f"Invalid input for variable 'dir_ind'")
 
-prev_margin=np.inf
-
-for i in np.arange(start_angle,end_angle,0.1): #going through every possible firing solution
-    eulers_output=eulers_method(1.275,initial_velocity,drag_coeff,rocket_radius,rocket_mass,sample_resolution,0,i) # [0] is time in flight (seconds), [1] is maximum horizontal range, [2] is x_array, [3] is y_array.  Both x & y arrays are displacement coordinates. [4] is "x" v, [5] is "y" v, [6] is y_max, [7] is y v, [8] is x v
+    for i in np.arange(start_angle,end_angle,.1): #going through every possible firing solution
+        prev_x_margin=np.inf
+        eulers_output=eulers_method(p,initial_velocity,drag_coeff,rocket_radius,rocket_mass,sample_resolution,y_displacement,i,end_elev) # [0] is time in flight (seconds), [1] is maximum horizontal range, [2] is x_array, [3] is y_array.  Both x & y arrays are displacement coordinates. [4] is "x" v, [5] is "y" v, [6] is y_max, [7] is y v, [8] is x v
+        for idx_element, x_element in enumerate(eulers_output[2]): #finding the closest coordinate to the ideal coordinate in the calculated trajectory
+            x_margin=abs(ideal_x-x_element)
+            if x_margin<prev_x_margin:
+                prev_x_margin=x_margin
+                closest_value_index=idx_element
+        
+        x_value=eulers_output[2][closest_value_index]
+        y_value=eulers_output[3][closest_value_index]
+        
+        margin=abs((ideal_x+ideal_y)-(x_value+y_value)) #finding the closest coordinate out of all possible trajectories' coordinates
+        if margin<prev_margin:
+            prev_margin=margin
+            theta=i
     
-    prev_x_margin=np.inf
+    eulers_output=eulers_method(p,initial_velocity,drag_coeff,rocket_radius,rocket_mass,sample_resolution,y_displacement,theta,end_elev) # [0] is time in flight (seconds), [1] is maximum horizontal range, [2] is x_array, [3] is y_array.  Both x & y arrays are displacement coordinates. [4] is "x" v, [5] is "y" v, [6] is y_max, [7] is y v, [8] is x v
+
+    if 1==1: #plotting graph 
+        fig, (ax1,ax2) = plt.subplots(1,2)
+        ax1.plot(eulers_output[2],eulers_output[3])
+        ax1.plot(ideal_x,ideal_y,marker="x", markersize=20, markeredgecolor="red", markerfacecolor="red")
+        plt.xlim([0,eulers_output[1]+5])
+        plt.ylim([0,eulers_output[6]+5])
+        ax2.plot(eulers_output[2],eulers_output[3])
+        ax2.plot(ideal_x,ideal_y,marker="x", markersize=15, markeredgecolor="red", markerfacecolor="red")
+        plt.xlim([0,eulers_output[1]+5])
+        plt.ylim([0,eulers_output[1]+5])
+        plt.show()
+    return theta, prev_margin
+
+def find_x_y(range,theta):
+    x=range*math.cos(math.radians(theta))
+    y=range*math.sin(math.radians(theta))
+    return x,y
+
+def find_new_delta_yaw_pitch(new_normalized_pitch,roll):
+    new_normalized_pitch=math.radians(new_normalized_pitch)
+    roll=math.radians(roll)
+    new_yaw=math.asin(math.tan(roll)*math.tan(math.asin(math.cos(roll)*math.sin(new_normalized_pitch)))) #derived equ.
+    delta_yaw=new_yaw #amount angle needs to change to get to new yaw location
     
-    for idx_element, x_element in enumerate(eulers_output[2]): #finding the closest coordinate to the ideal coordinate in the calculated trajectory
-        x_margin=abs(ideal_x-x_element)
-        if x_margin<prev_x_margin:
-            prev_x_margin=x_margin
-            closest_value_index=idx_element
+    new_pitch=math.asin(math.cos(roll)*math.sin(new_normalized_pitch)) #derived equ.
+    delta_pitch=new_pitch #amount angle needs to change to get to new pitch (NOT normalized, this value is ready-to-use) location
     
-    x_value=eulers_output[2][closest_value_index]
-    y_value=eulers_output[3][closest_value_index]
-
-    margin=abs((ideal_x+ideal_y)-(x_value+y_value)) #finding the closest coordinate out of all possible trajectories' coordinates
-    if margin<prev_margin:
-        prev_margin=margin
-        theta=i
-
-print(f"Angle from horizontal: {theta:.1f} degrees, Theoretical error: {prev_margin:.1f} meters")
-
-eulers_output=eulers_method(1.275,initial_velocity,drag_coeff,rocket_radius,rocket_mass,sample_resolution,0,theta)
-
-#fin_vel=np.sqrt(eulers_output[7]*eulers_output[7]+eulers_output[8]*eulers_output[8]) #this only tells you velocity at y=0
-#print(fin_vel)
-
-with open('Coordinate_Log.txt','w') as f:
-    f.write('Flight Time: %f seconds, Max Height: %f meters, Max Range: %f meters, (x,y) initial velocity: (%f,%f)|'%(eulers_output[0], eulers_output[6], eulers_output[1], eulers_output[4], eulers_output[5]))
-    f.write('\n\n')
-    f.write(str(eulers_output[2]))
-    f.write('\n\n')
-    f.write(str(eulers_output[3]))
-
-if 1==1: #plotting graph 
-    fig, (ax1,ax2) = plt.subplots(1,2)
-    ax1.plot(eulers_output[2],eulers_output[3])
-    ax1.plot(ideal_x,ideal_y,marker="x", markersize=20, markeredgecolor="red", markerfacecolor="red")
-    plt.xlim([0,eulers_output[1]+5])
-    plt.ylim([0,eulers_output[6]+5])
-    ax2.plot(eulers_output[2],eulers_output[3])
-    ax2.plot(ideal_x,ideal_y,marker="x", markersize=15, markeredgecolor="red", markerfacecolor="red")
-    plt.xlim([0,eulers_output[1]+5])
-    plt.ylim([0,eulers_output[1]+5])
-    plt.show()
+    return delta_yaw, delta_pitch 
+    #Note: if this direct method doesn't work, another solution is to use delta_yaw, but let the arduino determine
+    #delta_pitch based off the raw accelerometer measurement of pitch.  So just use pitch measurement to get as close
+    #to ideal pitch measurement (both normalized).
